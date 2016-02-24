d582d9d0b7a0a6f388975ef14a0e736773e54657
JustinLC <Justin_L.C@hotmail.com>
2016-02-24 22:53:48 +0000

    added actions

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,40 @@ end
 get '/signin' do
   erb :'user/signin'
 end
+
+get '/signout' do
+	session[:user_id] = nil
+	redirect '/login'
+end
+
+get '/signup' do
+	@user = User.new
+	erb :signup
+end
+
+post '/signup' do
+  @user = User.new
+  @user.name = params[:name]
+  @user.email = params[:email]
+  @user.password = params[:password]
+  @user.password_confirmation = params[:password_confirmation]
+
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :signup
+  end
+end
+
+post '/signin' do
+	name = params[:name]
+	email = params[:email]
+	password = params[:password]
+	user = User.find_by(name: name)
+	user = User.find_by(email: email)
+	if user.password == password
+		session[:user_id] = user.id
+		redirect '/'
+	end
+end
