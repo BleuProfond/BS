d399aba72927cfb4b44f4b2a68fd4ff7107b1dbe
BleuProfond <p.verteuil@gmail.com>
2016-02-24 23:04:16 +0000

    added project views

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,15 @@ end
 get '/signin' do
   erb :'user/signin'
 end
+
+get '/signin' do
+  erb :'user/signin'
+end
+
+get '/project' do
+  erb :'project/project'
+end
+
+get '/project/new' do
+  erb :'project/new'
+end
