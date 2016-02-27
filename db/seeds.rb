6c71be66bd5cf4341c5487f889716a43f498db1d
BleuProfond <p.verteuil@gmail.com>
2016-02-27 19:19:55 +0000

    improved seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,68 @@
 require 'faker'
 
 # Create main user
-user = User.create!(
-  name: 'Bob',
-  email: 'bob@example.com',
-  password: '1234',
+user1 = User.create!(
+  name: 'Bob Bentley',
+  email: 'bob@hotmail.com',
+  password: '12345',
 )
 
-user = User.create!(
-  name: 'Tom',
+user2 = User.create!(
+  name: 'Tom Harris',
   email: 'tom@gmail.com',
   password: '12345'
 )
 
-project = Project.create!(
-  project_name: 'Super Clothes',
-  project_idea: 'I want pants that fly',
-  project_problem: 'They won''t fly',
-  user_id: '1',
-  tags: "clothes, fly, pants"
-  )
+user3 = User.create!(
+  name: 'Cindy Lou',
+  email: 'cindy@yahoo.com',
+  password: '12345'
+)
 
-project = Project.create!(
-  project_name: 'Cookie Monster',
-  project_idea: 'Where can I find more cookies?',
-  project_problem: 'Can''t stop eating cookies please help!',
-  user_id: '2',
-  tags: "cookie, eating, monster"
-  )
+project1 = Project.create!(
+  project_name: 'Unstuck',
+  project_idea: 'A site where people can collaborate on projects, facilitate new ideas and solve technical challenges.',
+  project_problem: 'We want users to be able to create mind-maps for their projects.',
+  user_id: User.find(1).id,
+  tags: "brain, storm, solve"
+)
 
-project = Project.create!(
-  project_name: 'Gaming Addiction',
-  project_idea: 'Games to play while im sleeping.',
-  project_problem: 'Non-stop gaming 24/7. What do?',
-  user_id: '2',
-  tags: "game, sleep, addiction"
-  )
+project2 = Project.create!(
+  project_name: 'MusicPad',
+  project_idea: 'An online notepad for musical ideas.',
+  project_problem: 'I need to figure out the best way to implement a software synthesizer for users to work with on a browser.',
+  user_id: User.find(1).id,
+  tags: "music, synth, notes"
+)
+
+project3 = Project.create!(
+  project_name: 'Dungeon Blasters',
+  project_idea: 'A game that combines dungeon-crawlers with Bomberman.',
+  project_problem: 'I''m not sure what frameworks would be the most suited for making this kind of game.',
+  user_id: User.find(2).id,
+  tags: "game, dungeon, bomberman"
+)
+
+Comment1 = Comment.create!(
+  user_id: User.find(3).id,
+  project_id: Project.find(3).id,
+  comments: "You can try making it on Rails using the Gosu gem for your graphics."
+)
+
+Comment2 = Comment.create!(
+  user_id: User.find(2).id,
+  project_id: Project.find(2).id,
+  comments: "You will need to learn about websockets if you plan on trying to record live."
+)
+
+Comment3 = Comment.create!(
+  user_id: User.find(3).id,
+  project_id: Project.find(2).id,
+  comments: "You should checkout a library for Ruby called midilib. It will help you save a performance as a midi file which you could apply synths to later."
+)
+
+Comment4 = Comment.create!(
+  user_id: User.find(2).id,
+  project_id: Project.find(1).id,
+  comments: "You could implement the mind-map creation with Javascript. There are a couple API's available for making diagrams."
+)
